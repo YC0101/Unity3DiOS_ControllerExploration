16a7eefc75ee10de1acbc113fa44e9bb6d84e072
Yanxi Chen <103548668+YC0101@users.noreply.github.com>
2023-02-16 01:13:08 -0500

    Assets for this project

---
+++ Script/LeftMove.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LeftMove : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    bool isPressed = false;
+    public GameObject Player;
+    public float Force;
+    public float speed = 0;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = Player.GetComponent<Rigidbody>();
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            //Player.transform.Translate(Force * Time.deltaTime, 0, 0);
+            Vector3 movement = new Vector3(Force * Time.deltaTime, 0, 0);
+            rb.AddForce(movement*speed);
+        }
+        else
+        {
+            //Player.transform.Translate(0, 0, 0);
+        }
+    }
+}
